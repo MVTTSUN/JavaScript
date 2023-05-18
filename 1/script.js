4bc5e2d854912f6b48ea23c60a834442832485a0
MVTT <ryabtzun2011@yandex.ru>
2023-05-18 12:09:27 +0300

    finish 1 task

---
+++ 1/script.js
@@ -0,0 +1,9 @@
+// 1
+const CELSIUS = 30;
+const FAHRENHEIT = (9 / 5) * CELSIUS + 32;
+console.log(`Градус Цельсия: ${CELSIUS}\nГрадус Фаренгейта: ${FAHRENHEIT}`);
+
+// 2
+const name = 'Матвей';
+const admin = name;
+console.log(admin);
